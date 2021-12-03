932fe68304c8639b8be182de091b5d16178a80eb
Jayachandran <vcjayan@ymail.com>
2021-12-03 08:30:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+loaded_model = pickle.load(open('model_popchurn_new.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/result', methods = ['POST'])
+def result():
+    to_predict_list = request.form.to_dict()
+    to_predict_list = list(to_predict_list.values())
+    to_predict_list = list(map(float, to_predict_list))
+    to_predict = np.array(to_predict_list).reshape(1,4)
+    result = loaded_model.predict(to_predict)   
+    
+    return render_template('result.html', prediction=result)
+    
+    #if result == 1:
+        #return render_template('result.html', prediction_text = 'Warning..! The customer may churn')
+    #else:
+        #return render_template('result.html', prediction_text = 'Great..!! The customer will retain')
+
+if __name__ == "__main__":
+    app.run(debug = True)
